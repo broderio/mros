9c0983fcf4345a66da936a6e41005e08e7c7adbc
Broderick <broderio@umich.edu>
2024-07-19 12:36:17 -0400

    adds more library support

---
+++ mrosConfig.cmake
@@ -0,0 +1,9 @@
+# Get the directory containing this file.
+get_filename_component(MROS_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+# Set the locations of your libraries and headers.
+set(MROS_LIBRARIES "/usr/local/lib/mros")
+set(MROS_INCLUDE_DIRS "/usr/local/include/mros")
+
+# Load the settings into the current CMake project.
+include(${MROS_CURRENT_DIR}/mrosTargets.cmake)
