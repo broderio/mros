a082fac79c171d26a2d6a3e77a3193be3e2f17f8
Broderick <broderio@umich.edu>
2024-07-19 13:06:18 -0400

    updates cmake global library var

--- mrosConfig.cmake
+++ mrosConfig.cmake
@@ -1,8 +1,10 @@
 # Get the directory containing this file.
 get_filename_component(MROS_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
-# Set the locations of your libraries and headers.
-set(MROS_LIBRARIES "/usr/local/lib/mros")
+# Get a list of all library files in the /usr/local/lib/mros directory.
+file(GLOB MROS_LIBRARIES "/usr/local/lib/mros/*.a")
+
+# Set the location of your headers.
 set(MROS_INCLUDE_DIRS "/usr/local/include/mros")
 
 # Load the settings into the current CMake project.
